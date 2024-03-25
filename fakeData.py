5da610463ae026dbfb1fcda9b61928ea9c74bc44
Sander Bos <sanderb0ss@live.nl>
2024-03-25 15:44:08 +0100

    Uses strings, so that csv file is correct

--- fakeData.py
+++ fakeData.py
@@ -17,12 +17,13 @@ class fake_demand:
         data = []
         for _ in range(self.number):
             name = self.fake.name()
-            fake_address = self.fake.address()
+            fake_address = self.fake.postcode()
+            fake_city = self.fake.city()
             fake_email = self.fake.email()
             order_date = self.fake.date_between_dates(date_start=self.start_date, date_end =self.end_date)
             product = random.choice(self.products)
             amount = random.sample(range(10, 80), 1)
-            row = [name, fake_address, fake_email, order_date, product, amount[0]]
+            row = [f"{name}", f"{fake_address}", f"{fake_city}", f"{fake_email}", f"{order_date}", f"{product}", f"{amount[0]}"]
             data.append(row)
         return data
 
@@ -31,13 +32,15 @@ def main():
     """
         @number: how many times a fake row is created
         @start_date: The start date of the faked dataset
-        $end_date: The end date of the faked dataset
+        @end_date: The end date of the faked dataset
+        @products: define the products that are used by the dataset
+
     """
-    number = 100
+    number = 1000
     start_date = datetime(2023, 1, 1)
     end_date = datetime(2023, 12, 31)
     products = ['orange', 'apple', 'banana', 'pear']
-    location = 'data/mockdataset.csv'
+    location = 'data/mockdataset_2023.csv'
 
     fake_dataset_creator = fake_demand(number, start_date, end_date, products)
     dataset = fake_dataset_creator.mock_data()
