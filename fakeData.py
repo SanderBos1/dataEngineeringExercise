e5b385b01d06231b06ef02327c3800a24d6ebc2a
Sander Bos <sanderb0ss@live.nl>
2024-03-27 10:30:35 +0100

    Tweeked dataset composition

--- fakeData.py
+++ fakeData.py
@@ -5,11 +5,12 @@ import pandas as pd
 
 class fake_demand:
 
-    def __init__(self, number, start_date, end_date, products):
+    def __init__(self, number, start_date, end_date, products, cities):
         self.number = number
         self.start_date = start_date
         self.end_date = end_date
         self.products=products
+        self.cities = cities
         self.fake = Faker('nl_NL')
 
 
@@ -17,37 +18,38 @@ class fake_demand:
         data = []
         for _ in range(self.number):
             name = self.fake.name()
-            fake_address = self.fake.postcode()
-            fake_city = self.fake.city()
             fake_email = self.fake.email()
+            fake_city = random.choice(self.cities)
             order_date = self.fake.date_between_dates(date_start=self.start_date, date_end =self.end_date)
             product = random.choice(self.products)
             amount = random.sample(range(10, 80), 1)
-            row = [f"{name}", f"{fake_address}", f"{fake_city}", f"{fake_email}", f"{order_date}", f"{product}", f"{amount[0]}"]
+            row = [f"{name}", f"{fake_email}", f"{fake_city}", f"{order_date}", f"{product}", f"{amount[0]}"]
             data.append(row)
         return data
 
 
 def main():
     """
-        @number: how many times a fake row is created
-        @start_date: The start date of the faked dataset
-        @end_date: The end date of the faked dataset
-        @products: define the products that are used by the dataset
+        @number: How many times a fake row is created.
+        @start_date: The start date of the faked dataset.
+        @end_date: The end date of the faked dataset.
+        @products: Define the products that are used by the dataset.
+        @location: Where the csv file is saved.
+        @Cities: Where the order came from.
 
     """
-    number = 1000
+    number_rows = 3000
     start_date = datetime(2023, 1, 1)
     end_date = datetime(2023, 12, 31)
     products = ['orange', 'apple', 'banana', 'pear']
+    cities = ['Eindhoven', 'Delft', "Rotterdam", "Den Haag", "Middelburg"]
     location = 'data/mockdataset_2023.csv'
 
-    fake_dataset_creator = fake_demand(number, start_date, end_date, products)
+    fake_dataset_creator = fake_demand(number_rows, start_date, end_date, products, cities)
     dataset = fake_dataset_creator.mock_data()
     df = pd.DataFrame(dataset)
     df.to_csv(location)
-    print(df)
 
 
-
-main()
+if __name__ == '__main__':
+    main()
