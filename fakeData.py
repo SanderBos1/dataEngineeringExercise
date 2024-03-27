3b23c4b9953c1de367c509af1327a74596928c28
Sander Bos <sanderb0ss@live.nl>
2024-03-27 15:53:30 +0100

    Script improvement
    - moved location so that a user no longer has to specify this
    - add array years that defines how many datasets are created

--- fakeData.py
+++ fakeData.py
@@ -5,10 +5,9 @@ import pandas as pd
 
 class fake_demand:
 
-    def __init__(self, number, start_date, end_date, products, cities):
+    def __init__(self, number, years, products, cities):
         self.number = number
-        self.start_date = start_date
-        self.end_date = end_date
+        self.years = years
         self.products=products
         self.cities = cities
         self.fake = Faker('nl_NL')
@@ -16,39 +15,40 @@ class fake_demand:
 
     def mock_data(self):
         data = []
-        for _ in range(self.number):
-            name = self.fake.name()
-            fake_email = self.fake.email()
-            fake_city = random.choice(self.cities)
-            order_date = self.fake.date_between_dates(date_start=self.start_date, date_end =self.end_date)
-            product = random.choice(self.products)
-            amount = random.sample(range(10, 80), 1)
-            row = [f"{name}", f"{fake_email}", f"{fake_city}", f"{order_date}", f"{product}", f"{amount[0]}"]
-            data.append(row)
-        return data
+        for year in self.years:
+            start_date = datetime(year, 1, 1)
+            end_date = datetime(year, 12, 31)
+            location = f'data/mockdataset_{year}.csv'
+            for _ in range(self.number):
+                name = self.fake.name()
+                fake_email = self.fake.email()
+                fake_city = random.choice(self.cities)
+                order_date = self.fake.date_between_dates(date_start=start_date, date_end =end_date)
+                product = random.choice(self.products)
+                amount = random.randint(-1, 100)
+                row = [f"{name}", f"{fake_email}", f"{fake_city}", f"{order_date}", f"{product}", f"{amount}"]
+                data.append(row)
+            df = pd.DataFrame(data)
+            df.to_csv(location)
 
 
 def main():
     """
         @number: How many times a fake row is created.
-        @start_date: The start date of the faked dataset.
-        @end_date: The end date of the faked dataset.
+        @years: a list of years, for each year a dataset is created.
         @products: Define the products that are used by the dataset.
         @location: Where the csv file is saved.
         @Cities: Where the order came from.
 
     """
     number_rows = 3000
-    start_date = datetime(2023, 1, 1)
-    end_date = datetime(2023, 12, 31)
+    years = [2021, 2022,2023]
     products = ['orange', 'apple', 'banana', 'pear']
     cities = ['Eindhoven', 'Delft', "Rotterdam", "Den Haag", "Middelburg"]
-    location = 'data/mockdataset_2023.csv'
 
-    fake_dataset_creator = fake_demand(number_rows, start_date, end_date, products, cities)
-    dataset = fake_dataset_creator.mock_data()
-    df = pd.DataFrame(dataset)
-    df.to_csv(location)
+    fake_dataset_creator = fake_demand(number_rows, years, products, cities)
+    fake_dataset_creator.mock_data()
+
 
 
 if __name__ == '__main__':
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Product dataset generator
+
+This python script is written to be used as a random dataset generator.
+The dataset that is generated simulates a company that sells different products to customers in different cities.
+In the main function the years, cities, products and number of rows can be defined.
+The dataset generated this way is a great way to practice anything that uses datasets.
